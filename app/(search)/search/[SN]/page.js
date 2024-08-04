097ece2c68ba974e6e9cc62a15d8e035e73477a6
harshj3915 <hjain3915@gmail.com>
2024-08-04 20:48:52 +0530

    refactor: Remove unused import and fix formatting in Home component

--- app/(search)/search/[SN]/page.js
+++ app/(search)/search/[SN]/page.js
@@ -1,6 +1,5 @@
 "use client"
 import { useState, useEffect } from "react";
-import INTRO from "@/components/intro";
 import NavbarDark from "@/components/navbar";
 import WeatherPage from "@/components/tempinfo";
 
@@ -46,7 +45,7 @@ export default function Home({ params }) {
       } catch (error) {
         console.error("Error fetching weather data:", error);
         setWeatherData(null);
-      }finally{
+      } finally {
         setLoading(false)
       }
     };
@@ -55,48 +54,48 @@ export default function Home({ params }) {
     fetchWeatherData();
   }, [params.SN]);
 
-  return ( <div style={{ position: "absolute" }} className="absolute inset-0 bg-cover bg-center bg-no-repeat">
-  {loading ? (
-    <div className="flex justify-center items-center h-full">
-      <p>Loading...</p>
-    </div>
-  ) : (
-    <>
-      <div
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          background: "linear-gradient(to bottom, rgba(4,47,102,0.7), rgba(4,47,102,1))",
-          zIndex: -2,
-        }}
-      ></div>
-      <div
-        style={{
-          backgroundImage: `url(${backgroundImage})`,
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          backgroundPosition: "center",
-          filter: "blur(3px)",
-          zIndex: -1,
-          backgroundSize: "cover",
-        }}
-      ></div>
-      <NavbarDark />
-      {weatherData ? (
-        <WeatherPage weatherData={weatherData} />
-      ) : (
-        <div className="text-white">
-          <p>Weather data is taking too long to load. Please wait</p>
-        </div>
-      )}
-    </>
-  )}
-</div>
-);
+  return (<div style={{ position: "absolute" }} className="absolute inset-0 bg-cover bg-center bg-no-repeat">
+    {loading ? (
+      <div className="flex justify-center items-center h-full">
+        <p>Loading...</p>
+      </div>
+    ) : (
+      <>
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            background: "linear-gradient(to bottom, rgba(4,47,102,0.7), rgba(4,47,102,1))",
+            zIndex: -2,
+          }}
+        ></div>
+        <div
+          style={{
+            backgroundImage: `url(${backgroundImage})`,
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundPosition: "center",
+            filter: "blur(3px)",
+            zIndex: -1,
+            backgroundSize: "cover",
+          }}
+        ></div>
+        <NavbarDark />
+        {weatherData ? (
+          <WeatherPage weatherData={weatherData} />
+        ) : (
+          <div className="text-white">
+            <p>Weather data is taking too long to load. Please wait</p>
+          </div>
+        )}
+      </>
+    )}
+  </div>
+  );
 };
--- components/intro.js
+++ components/intro.js
@@ -6,7 +6,7 @@ export default function INTRO() {
         <div className=" max-w-7xl mx-auto my-16 text-center text-white p-6 bg-gray-800 rounded-lg">
           <h2 className="text-4xl md:text-5xl font-bold mb-8">Welcome to ClimateCraft</h2>
           <p className="text-lg md:text-xl text-gray-300 mb-12">Get real-time weather updates from around the world.</p>
-          <p className="text-sm md:text-base text-gray-400">Created by: <span className="font-bold">Harsh Kothari - 22BCE3655<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Harsh Gupta - 22BCB0169</span></p>
+          <p className="text-sm md:text-base text-gray-400">Created by: <span className="font-bold">Harsh Kothari - 22BCE3655<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></p>
         </div>
         
         <div className="mt-[85px] text-center text-white">
